7ce95c5aba622812a5ebb5d4239722982420ee60
AndrewCJohnson90 <andrew.c.johnson90@gmail.com>
2019-01-22 14:05:49 -0800

    Add files via upload

--- RAWS_Analysis_NortheastCaliforina.R
+++ RAWS_Analysis_NortheastCaliforina.R
@@ -6,7 +6,7 @@ library(mesowest)
 ### Script for loading data directly from the mesowest API, .csv is not saved locally
 
 # Sign up for Mesowest and recieve an API Key here: https://developers.synopticdata.com/signup/
-requestToken(apikey = "INSERTAPIKEY")
+requestToken(apikey = "KzsQxP3c0dI4YX2Rs1wpYyz1hHKcvZujmBb")
 
 #Get current year: 
 CurrentYear = format(Sys.Date(), "%Y")
@@ -21,36 +21,42 @@ CurrentEndDate = paste(CurrentYear,CurrentMonth,'010000',sep = "")
 RVDC1_01_11 = mw(service = 'precipitation',stid='RVDC1',start='200107010000',end='201106010000',units='english',pmode="intervals", interval="month")
 RVDC1_01_11_DF = as.data.frame(RVDC1_01_11$STATION$OBSERVATIONS$precipitation)
 RVDC1_01_11_DF$Station <- 'RVDC1'
+RVDC1_01_11_DF$StationName <- RVDC1_01_11$STATION$NAME
 
 RVDC1_11_18 = mw(service = 'precipitation',stid='RVDC1',start='201109010000',end= CurrentEndDate,units='english',pmode="intervals", interval="month")
 RVDC1_11_18_DF = as.data.frame(RVDC1_11_18$STATION$OBSERVATIONS$precipitation)
 RVDC1_11_18_DF$Station <- 'RVDC1'
+RVDC1_11_18_DF$StationName <- RVDC1_11_18$STATION$NAME
+
 
 #Honey Lake - Missing 10/2006 - 11/2006
 HLKC1_00_06 = mw(service = 'precipitation',stid='HLKC1',start='200111010000',end='200610010000',units='english',pmode="intervals", interval="month")
 HLKC1_00_06_DF = as.data.frame(HLKC1_00_06$STATION$OBSERVATIONS$precipitation)
 HLKC1_00_06_DF$Station <- 'HLKC1'
+HLKC1_00_06_DF$StationName <- HLKC1_00_06$STATION$NAME
 
 HLKC1_06_18 = mw(service = 'precipitation',stid='HLKC1',start='200611010000',end= CurrentEndDate,units='english',pmode="intervals", interval="month")
 HLKC1_06_18_DF = as.data.frame(HLKC1_06_18$STATION$OBSERVATIONS$precipitation)
 HLKC1_06_18_DF$Station <- 'HLKC1'
-
+HLKC1_06_18_DF$StationName <- HLKC1_06_18$STATION$NAME
 
 #Hidden Valley - missing 10/2010 - 09/2011
 HDVC1_07_10 = mw(service = 'precipitation',stid='HDVC1',start='200711010000',end='201010010000',units='english',pmode="intervals", interval="month")
 HDVC1_07_10_DF = as.data.frame(HDVC1_07_10$STATION$OBSERVATIONS$precipitation)
 HDVC1_07_10_DF$Station <- 'HDVC1'
+HDVC1_07_10_DF$StationName <- HDVC1_07_10$STATION$NAME
 
 HDVC1_11_18 = mw(service = 'precipitation',stid='HDVC1',start='201109010000',end=CurrentEndDate ,units='english',pmode="intervals", interval="month")
 HDVC1_11_18_DF = as.data.frame(HDVC1_11_18$STATION$OBSERVATIONS$precipitation)
 HDVC1_11_18_DF$Station <- 'HDVC1'
-
+HDVC1_11_18_DF$StationName <- HDVC1_11_18$STATION$NAME
 
 
 #Bull Flat - 
 BUFC1All = mw(service = 'precipitation',stid='BUFC1',start='200106010000',end=CurrentEndDate ,units='english',pmode="intervals", interval="month")
 BUFC1 = as.data.frame(BUFC1All$STATION$OBSERVATIONS$precipitation)
 BUFC1$Station <- 'BUFC1'
+BUFC1$StationName <- BUFC1All$STATION$NAME
 
 
 
@@ -58,27 +64,37 @@ BUFC1$Station <- 'BUFC1'
 DYLC1_00_06= mw(service = 'precipitation',stid='DYLC1',start='200011010000',end='200612010000',units='english',pmode="intervals", interval="month")
 DYLC1_00_06_DF = as.data.frame(DYLC1_00_06$STATION$OBSERVATIONS$precipitation)
 DYLC1_00_06_DF$Station <- 'DYLC1'
+DYLC1_00_06_DF$StationName <- DYLC1_00_06$STATION$NAME
 
 DYLC1_08_18= mw(service = 'precipitation',stid='DYLC1',start='200802010000',end=CurrentEndDate ,units='english',pmode="intervals", interval="month")
 DYLC1_08_18_DF = as.data.frame(DYLC1_08_18$STATION$OBSERVATIONS$precipitation)
 DYLC1_08_18_DF$Station <- 'DYLC1'
+DYLC1_08_18_DF$StationName <- DYLC1_08_18$STATION$NAME
 
 #Buffalo Creek
 BUFN2All = mw(service = 'precipitation',stid='BUFN2',start='200106010000',end=CurrentEndDate ,units='english',pmode="intervals", interval="month")
 BUFN2 = as.data.frame(BUFN2All$STATION$OBSERVATIONS$precipitation)
 BUFN2$Station <- 'BUFN2'
+BUFN2$StationName <- BUFN2All$STATION$NAME
 
 #Grasshopper
 GRSC1All= mw(service = 'precipitation',stid='GRSC1',start='200106010000',end=CurrentEndDate ,units='english',pmode="intervals", interval="month")
 GRSC1 = as.data.frame(GRSC1All$STATION$OBSERVATIONS$precipitation)
 GRSC1$Station <- 'GRSC1'
+GRSC1$StationName <- GRSC1All$STATION$NAME
 
 AllStations <-rbind(BUFC1,BUFN2,DYLC1_08_18_DF,DYLC1_00_06_DF,GRSC1,HDVC1_11_18_DF,HDVC1_07_10_DF,HLKC1_00_06_DF,HLKC1_06_18_DF,RVDC1_01_11_DF,RVDC1_11_18_DF)
+
+
 AllStationsSplit <- strsplit(AllStations$first_report, "-")
 
 AllStations$year <- sapply(AllStationsSplit, "[", 1)
 AllStations$month <- sapply(AllStationsSplit, "[", 2)
 
+AllStations <- AllStations[complete.cases(AllStations),]
+
+
+##Water Year summary and charts
 #Water Year Function calculates water year for summary purposes
 wtr_yr <- function(dates, start_month = 10){ 
   # Convert possible character vector into date
@@ -93,46 +109,62 @@ wtr_yr <- function(dates, start_month = 10){
 AllStationsWaterYear <- wtr_yr(AllStations$first_report)
 AllStations$waterYear <- AllStationsWaterYear
 
-    
-library(dplyr)
-AllStationsSummary = AllStations %>% 
-  group_by_(.dots=c("Station","waterYear")) %>% 
-  summarize(x=sum(total))
 
+library(dplyr)
+WaterYearSummary = AllStations %>% 
+  group_by_(.dots=c("StationName","waterYear")) %>% 
+  summarize(x=sum(total))
 
 library(ggplot2)
 # line with each station and all years
-p <- ggplot(data = AllStationsSummary,
-            mapping = aes(x = waterYear, color = Station))+ xlab("Water Year")
+p <- ggplot(data = WaterYearSummary,
+            mapping = aes(x = waterYear, color = StationName))+ xlab("Water Year")
 p + geom_line(mapping = aes(y = x),size = 1.5)+ ylab("Precipitation in Inches") 
 
 #Chart for each Station stacked vertically
-d <- ggplot(data = AllStationsSummary,
-            mapping = aes(x = waterYear, fill = Station ))
+d <- ggplot(data = WaterYearSummary,
+            mapping = aes(x = waterYear, fill = StationName ))
 d + geom_bar(position = "dodge",stat="identity",
              mapping = aes(y = x))+
-             facet_grid(Station ~ .  )+ ylab("Precipitation in Inches") + xlab("Water Year")+
+             facet_grid(StationName ~ .  )+ ylab("Precipitation in Inches") + xlab("Water Year")+
              theme( axis.text.y=element_text(angle=45)) + scale_x_continuous(breaks=seq(2000, 2019, 2))+ggtitle(paste("Annual Precipitation(in) for RAWS Stations from 2000-", CurrentYear,sep = ""))
 
 ## Chart for each year 
-c <- ggplot(data = AllStationsSummary,
-            mapping = aes(x = factor(waterYear), fill = Station))+
+c <- ggplot(data = WaterYearSummary,
+            mapping = aes(x = factor(waterYear), fill = StationName))+
             geom_bar(position = "dodge",stat="identity",
             mapping = aes(y = x))+
             facet_wrap(~ waterYear, ncol = 5, scales='free_x')+ ylab("Precipitation in Inches") + xlab("RAWS Station ID")+
             theme( axis.text.y=element_text(angle=45))+ggtitle(paste("Annual Precipitation(in) for Water Year (Oct-Nov)"))
 
 ## individual chart for 2019 one color pallet
-f <- ggplot(data = AllStationsSummary[AllStationsSummary$waterYear == 2019,],
-            mapping = aes(x = Station, fill = Station))
+f <- ggplot(data = WaterYearSummary[WaterYearSummary$waterYear == 2019,],
+            mapping = aes(x = StationName, fill = StationName))
 f + geom_bar(position = "dodge",stat="identity",
              mapping = aes(y = x))+ ylab("Precipitation in Inches") + xlab("RAWS Station ID") + scale_fill_brewer(palette="Reds")  # "Reds" is palette name
 
 ## individual chart for 2012 many colors
-g <- ggplot(data = AllStationsSummary[AllStationsSummary$waterYear == 2012,],
-            mapping = aes(x = Station, fill = Station))
+g <- ggplot(data = WaterYearSummary[WaterYearSummary$waterYear == 2012,],
+            mapping = aes(x = StationName, fill = StationName))
 g + geom_bar(position = "dodge",stat="identity",
              mapping = aes(y = x)) + ylab("Precipitation in Inches") + xlab("RAWS Station ID") +ggtitle(paste(2012,": Annual Precipitation (in) for Water Year (Oct-Nov)"))
+
+#Monthly Summary
+MonthlySummary = AllStations %>% 
+  group_by_(.dots=c("StationName","year","month")) %>% 
+  summarize(x=sum(total))
+MonthlySummary$YearMonth <- as.yearmon(paste(MonthlySummary$year, MonthlySummary$month), "%Y %m")
+
+
+#MonthlySummary$YearMonth <- with(MonthlySummary, as.integer(sprintf("%s%02s", year, month)))
+
+#Chart for each Station stacked vertically
+d <- ggplot(data = MonthlySummary,
+            mapping = aes(x = YearMonth, fill = StationName ))
+d + geom_bar(position = "dodge",stat="identity",
+             mapping = aes(y = x))+
+  facet_grid(StationName ~ .  )+ ylab("Precipitation in Inches") + xlab("Year and Month")+
+  theme( axis.text.y=element_text(angle=45)) + scale_x_continuous(breaks=seq(2000, 2019, 2))+ggtitle(paste("Annual Precipitation(in) for RAWS Stations from 2000-", CurrentYear,sep = ""))
 
 
 
